f58c00d86c2b7ea9a1b6a64e7417573ed8400e8d
Henrique Ducati <henrique.dcti@gmail.com>
2021-09-16 18:01:15 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8e02004eee9c27f8fedbe4cb688b4551'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '19435f74-335d-4f1b-be0e-0223da753156'
+};
